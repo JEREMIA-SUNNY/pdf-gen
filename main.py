1b78bf633c61feef794234e8e474d32fe37bb822
JEREMIA-SUNNY <jeremiasunnylap@gmail.com>
2025-04-01 20:13:07 +0530

    next set of changes

--- main.py
+++ main.py
@@ -21,5 +21,54 @@ def generate_pdf():
     pdf_buffer.seek(0)
     return send_file(pdf_buffer, mimetype='application/pdf')
 
+@app.route('/generateCable-pdf/cable',methods=['POST'])
+def generateCable_pdf():
+
+    data=request.json
+
+    rendered_html=render_template("cable/cable.html", data=data)
+    pdf_buffer = io.BytesIO()
+    HTML(string=rendered_html).write_pdf(pdf_buffer)
+    pdf_buffer.seek(0)
+    return send_file(pdf_buffer, mimetype='application/pdf')
+
+
+@app.route('/generateMulti-pdf/Multi',methods=['POST'])
+def generateMulti_pdf():
+
+    data=request.json
+
+    rendered_html=render_template("multi-interface/multi-interface.html", data=data)
+    pdf_buffer = io.BytesIO()
+    HTML(string=rendered_html).write_pdf(pdf_buffer)
+    pdf_buffer.seek(0)
+    return send_file(pdf_buffer, mimetype='application/pdf')
+
+@app.route('/compliancePDF/compliance',methods=['POST'])
+def generateCompliance_pdf():
+
+    data=request.json
+
+    rendered_html=render_template("compliance/compliance.html", data=data)
+    pdf_buffer = io.BytesIO()
+    HTML(string=rendered_html).write_pdf(pdf_buffer)
+    pdf_buffer.seek(0)
+    return send_file(pdf_buffer, mimetype='application/pdf')
+
+@app.route('/quotes/quotes',methods=['POST'])
+def generateQuotes_pdf():
+
+    data=request.json
+
+    rendered_html=render_template("quotes/quotes.html", data=data)
+    pdf_buffer = io.BytesIO()
+    HTML(string=rendered_html).write_pdf(pdf_buffer)
+    pdf_buffer.seek(0)
+    return send_file(pdf_buffer, mimetype='application/pdf')
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
